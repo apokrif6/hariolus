0e38c59e42e2a70a009d48d406987e8cba20311b
Zhenia <j.berenchan@gmail.com>
2022-08-09 18:16:21 +0200

    created prefab generator mechanism

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,10 @@ namespace Engine
     {
         public static GameManager GameManagerInstance { get; private set; }
         public GameObject playerController;
+        public GameObject[] stationaryPatientsPlaces;
         
         private bool _firstAwake;
+        private GameObject _patientPrefab;
         private void Awake()
         {
             if (GameManagerInstance != null && GameManagerInstance != this)
@@ -18,15 +20,43 @@ namespace Engine
             GameManagerInstance = this;
 
             _firstAwake = true;
+            _patientPrefab = null;
         }
 
         private void Update()
         {
             if (_firstAwake)
             {
+                DayNightSystem.Instance.ChangeHour += HourLapse;
                 HerbalismManager.Instance.LoadData();
                 _firstAwake = false;
             }
+        }
+
+        private void HourLapse(int Hour)
+        {
+            foreach (GameObject patientPlace in stationaryPatientsPlaces)
+            {
+                if (patientPlace.transform.childCount == 0 && UnityEngine.Random.Range(0, 2) == 0)
+                {
+                    CreatePatient(patientPlace);
+                }
+            }
+        }
+
+        private void CreatePatient(GameObject creationPlace)
+        {
+            if (_patientPrefab == null)
+                _patientPrefab = Resources.Load<GameObject>("Prefabs/Patient");
+
+            if (_patientPrefab == null)
+                Debug.LogError("Patient Prefab is missing");
+            else
+            {
+                GameObject newPatient = Instantiate(_patientPrefab, Vector3.zero, Quaternion.identity, creationPlace.transform);
+                newPatient.transform.localPosition = Vector3.zero;
+            }
+
         }
     }
 }
--- Assets/Scripts/PatientComponent.cs
+++ Assets/Scripts/PatientComponent.cs
@@ -5,11 +5,14 @@ namespace Engine
 {
     public class PatientComponent: MonoBehaviour
     {
+        public const int MAX_DISEASES = 3;
         public Patient PrivatePatient { get; private set; }
 
         private void Start()
         {
             PrivatePatient = new Patient();
+            DayNightSystem.Instance.ChangeHour += HourLapse;
+            DayNightSystem.Instance.ChangeMinute += MinuteLapse;
         }
 
         private void Update()
@@ -19,7 +22,42 @@ namespace Engine
             Vector3 lookTarget = new Vector3(position.x, transform.position.y, position.z);
             
             Quaternion lookDirection = Quaternion.LookRotation(lookTarget - transform.position, transform.up);
+            
             transform.rotation = lookDirection;
+        }
+
+        private void HourLapse(int hour)
+        {
+            if (PrivatePatient.NumberCurrentEvents < MAX_DISEASES)
+            {
+                MedicalEvent newMedicalEvent = HerbalismManager.Instance.CreatePrimaryMedicalEvent();
+
+                if (newMedicalEvent != null)
+                {
+                    PrivatePatient.AddNewEvent(newMedicalEvent);
+                }
+            }
+            
+            if (PrivatePatient.NumberCurrentEvents > 0)
+                gameObject.SetActive(true);
+        }
+        
+        private void MinuteLapse(int minute)
+        {
+            if (PrivatePatient.NumberCurrentEvents == 0)
+            {
+                MedicalEvent medicalEvent = HerbalismManager.Instance.CreatePrimaryMedicalEvent();
+
+                if (medicalEvent == null)
+                {
+                    gameObject.SetActive(false);
+                }
+                else
+                {
+                    PrivatePatient.AddNewEvent(medicalEvent);
+                    gameObject.SetActive(true);
+                }
+            }
         }
     }
 }
